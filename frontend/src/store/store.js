859aaade29b8f3e508e463bcb49038c22a357583
semihkececioglu <semihkecec@gmail.com>
2025-10-27 00:10:12 +0300

    refactor: update middleware configuration to enable serializableCheck with specific ignored actions and paths

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -7,9 +7,15 @@ export const store = configureStore({
     auth: authReducer,
     chat: chatReducer,
   },
-  middleware: (getDefaultMiddleware) => {
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
-    });
-  },
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["your/action/type"],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ["meta.arg", "payload.timestamp"],
+        // Ignore these paths in the state
+        ignoredPaths: ["items.dates"],
+      },
+    }),
 });
